2c9e4f3f7736ff8f81640b52ed590ac7e099e2ee
Josh Loecker <joshloecker@icloud.com>
2023-01-25 10:53:28 -0600

    Included an "execution" script
    This will allow for running snakemake until a successful error code has been reached (`0`). This can be dangerous as it will not stop until this code is reached

---
+++ execute.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+execution_successful=false
+profile=$1
+
+# If `profile` is empty, set `profile` to "slurm"
+if [ -z "$profile" ]; then
+    profile="slurm"
+fi
+
+
+# While not execution_successful
+while [ $execution_successful == false ]; do
+  snakemake --profile $profile
+  error_code=$?
+
+  # Execute snakemake again if error code is not 0
+  if [ $error_code == 0 ]; then
+    execution_successful=true
+  fi
+done
