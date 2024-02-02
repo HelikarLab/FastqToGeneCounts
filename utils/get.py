21f7dd6bf8877966e49fe0f2860de680903850b8
Josh Loecker <joshloecker@icloud.com>
2024-02-02 13:35:08 -0600

    Updated to match new config.yaml file

--- utils/get.py
+++ utils/get.py
@@ -1,47 +1,51 @@
-import os
 import csv
+import os
 import sys
-import snakemake
 from pathlib import Path
-from . import perform
-from .constants import Layout
 from typing import Union
+
+import snakemake
+from snakemake import io
+
+from utils import perform
+from utils.constants import Layout
+
 
 def from_master_config(config: dict, attribute: str) -> list[str]:
     valid_inputs = ["SRR", "tissue", "tag", "PE_SE"]
     sub_list = ["tissue", "tag"]
     if attribute not in valid_inputs:
         sys.exit(f"\nInvalid attribute input. '{attribute}' is not one of: {valid_inputs}\n")
-
+    
     collect_attributes = []
     index_value = valid_inputs.index(attribute)
-
+    
     # We have to subtract one because "tissue" and "tag" are in the same index, thus the index value in valid_inputs is increased by one
     if index_value >= 2:
         index_value -= 1
-
+    
     control_lines = open(config["MASTER_CONTROL"], "r")
     dialect = csv.Sniffer().sniff(control_lines.read(1024))
     control_lines.seek(0)
     reader = csv.reader(control_lines, delimiter=str(dialect.delimiter))
-
+    
     for line in reader:
-
+        
         # Get the column from master_control we are interested in
         column_value = line[index_value]
         PE_SE_value = Layout[line[2]]  # PE, SE, or SLC
-
+        
         # test if we are looking for "tissue" or "tag", as these two values are located at master_control index 1
         if attribute in sub_list:
             sub_index = sub_list.index(attribute)
             split_list = str(line[index_value]).split("_")
-
+            
             # We must append the target attribute twice if it is paired end, once if it is single end
             if PE_SE_value in [Layout.PE, Layout.SLC]:
                 target_attribute = [split_list[sub_index], split_list[sub_index]]
             elif PE_SE_value == Layout.SE:
                 target_attribute = [split_list[sub_index]]
-
+        
         # Test if we are gathering the ended-ness (PE, SE, SLC)
         elif attribute == "PE_SE":
             # We must append the target attribute twice if it is paired end, once if it is single end
@@ -49,16 +53,16 @@ def from_master_config(config: dict, attribute: str) -> list[str]:
                 target_attribute = ["1", "2"]
             elif column_value == Layout.SE.name:  # Single end
                 target_attribute = ["S"]
-
+        
         # If we are doing anything else, simply append the column value the appropriate number of times
         else:
             if PE_SE_value in [Layout.PE, Layout.SLC]:
                 target_attribute = [line[index_value], line[index_value]]
             elif PE_SE_value == Layout.SE:
                 target_attribute = [line[index_value]]
-
+        
         collect_attributes += target_attribute
-
+    
     control_lines.close()
     return collect_attributes
 
@@ -75,7 +79,8 @@ def tissue_name(config: dict) -> list[str]:
     if perform.prefetch(config=config):
         return from_master_config(config=config, attribute="tissue")
     else:
-        fastq_input = snakemake.io.glob_wildcards(os.path.join(config["DUMP_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
+        fastq_input = snakemake.io.glob_wildcards(
+            os.path.join(config["LOCAL_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
         return fastq_input.tissue_name
 
 
@@ -83,7 +88,8 @@ def tags(config: dict) -> list[str]:
     if perform.prefetch(config=config):
         return from_master_config(config=config, attribute="tag")
     else:
-        fastq_input = snakemake.io.glob_wildcards(os.path.join(config["DUMP_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
+        fastq_input = snakemake.io.glob_wildcards(
+            os.path.join(config["LOCAL_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
         return fastq_input.tag
 
 
@@ -91,8 +97,10 @@ def PE_SE(config: dict) -> list[str]:
     if perform.prefetch(config=config):
         return from_master_config(config=config, attribute="PE_SE")
     else:
-        fastq_input = snakemake.io.glob_wildcards(os.path.join(config["DUMP_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
+        fastq_input = snakemake.io.glob_wildcards(
+            os.path.join(config["LOCAL_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
         return fastq_input.PE_SE
+
 
 def tag_from_filename(file_path: Union[str, Path]) -> str:
     file_name = os.path.basename(file_path)
@@ -100,19 +108,22 @@ def tag_from_filename(file_path: Union[str, Path]) -> str:
     tag = purge_extension.split("_")[-1]
     return str(tag)
 
+
 def direction_from_name(file: str):
     file_name = os.path.basename(file)
     purge_extension = file_name.split(".")[0]
     direction = purge_extension.split("_")[-1]
     return direction
 
+
 def sample(config: dict) -> list[str]:
     if perform.prefetch(config=config):
         tag = from_master_config(config=config, attribute="tag")
     else:
-        fastq_input = snakemake.io.glob_wildcards(os.path.join(config["DUMP_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
+        fastq_input = snakemake.io.glob_wildcards(
+            os.path.join(config["LOCAL_FASTQ_FILES"], "{tissue_name}_{tag}_{PE_SE}.fastq.gz"))
         tag = fastq_input.tag
-
+    
     sample = []
     for t in tag:
         sample.append(t.split("R")[0])
